e1debb089d5548f9319b016335a64f2e45d16b59
snayan <snayan@sina.com>
2018-06-27 18:16:56 +0800

    upgrade themes

--- themes/yilia/webpack.config.js
+++ themes/yilia/webpack.config.js
@@ -1,4 +1,4 @@
-var webpack = require("webpack");
+var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -10,46 +10,54 @@ var CleanPlugin = require('clean-webpack-plugin');
 var minifyHTML = {
   collapseInlineTagWhitespace: true,
   collapseWhitespace: true,
-  minifyJS:true
-}
+  minifyJS: true,
+};
+
+process.env.NODE_ENV = process.argv[2] === '-p' ? 'production' : 'development';
 
 module.exports = {
   entry: {
-    main: "./source-src/js/main.js",
-    slider: "./source-src/js/slider.js",
-    mobile: "./source-src/js/mobile.js"
+    main: './source-src/js/main.js',
+    slider: './source-src/js/slider.js',
+    mobile: './source-src/js/mobile.js',
   },
   output: {
-    path: "./source",
-    publicPath: "./",
-    filename: "[name].[chunkhash:6].js"
+    path: './source',
+    publicPath: './',
+    filename: '[name].[chunkhash:6].js',
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/
-    }, {
-      test: /\.html$/,
-      loader: 'html'
-    }, {
-      test: /\.(scss|sass|css)$/,
-      loader: ExtractTextPlugin.extract('style-loader', ['css-loader?-autoprefixer', 'postcss-loader', 'sass-loader'])
-    }, {
-      test: /\.(gif|jpg|png)\??.*$/,
-      loader: 'url-loader?limit=500&name=img/[name].[ext]'
-    }, {
-      test: /\.(woff|svg|eot|ttf)\??.*$/,
-      loader: "file-loader?name=fonts/[name].[hash:6].[ext]"
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loader: 'html',
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        loader: ExtractTextPlugin.extract('style-loader', ['css-loader?-autoprefixer', 'postcss-loader', 'sass-loader']),
+      },
+      {
+        test: /\.(gif|jpg|png)\??.*$/,
+        loader: 'url-loader?limit=500&name=img/[name].[ext]',
+      },
+      {
+        test: /\.(woff|svg|eot|ttf)\??.*$/,
+        loader: 'file-loader?name=fonts/[name].[hash:6].[ext]',
+      },
+    ],
   },
   alias: {
-    'vue$': 'vue/dist/vue.js'
+    vue$: 'vue/dist/vue.js',
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.common.js'
-    }
+      vue$: 'vue/dist/vue.common.js',
+    },
   },
   // devtool: '#eval-source-map',
   postcss: function() {
@@ -58,40 +66,40 @@ module.exports = {
   plugins: [
     new ExtractTextPlugin('[name].[chunkhash:6].css'),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
+      'process.env.NODE_ENV': '"production"',
     }),
     new HtmlWebpackPlugin({
       inject: false,
       cache: false,
       minify: minifyHTML,
       template: './source-src/script.ejs',
-      filename: '../layout/_partial/script.ejs'
+      filename: '../layout/_partial/script.ejs',
     }),
     new HtmlWebpackPlugin({
       inject: false,
       cache: false,
       minify: minifyHTML,
       template: './source-src/css.ejs',
-      filename: '../layout/_partial/css.ejs'
-    })
+      filename: '../layout/_partial/css.ejs',
+    }),
   ],
-  watch: true
-}
+  watch: false,
+};
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = 'none';
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
-      }
+        NODE_ENV: '"production"',
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings: false
-      }
+        warnings: false,
+      },
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new CleanPlugin('builds')
-  ])
+    new CleanPlugin('source'),
+  ]);
 }
