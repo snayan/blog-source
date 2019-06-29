345ab2b86d1f5a60bfa73ef3925e39d0424620a5
yang.zhang <yang.zhang@shopee.com>
2019-06-29 15:59:44 +0800

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,7 +78,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-142851521-1`,
       },
     },
     `gatsby-plugin-feed`,
